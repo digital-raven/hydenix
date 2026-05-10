eccb638f2157ed0fc73079317dc6694fc6e2e010
digital-raven <digital-raven@users.noreply.github.com>
2026-05-10 02:35:27 +0000

    chore(deps): update flake.lock

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Hydenix's nixpkgs
-    hydenix-nixpkgs.url = "github:nixos/nixpkgs/12a55407652e04dcf2309436eb06fef0d3713ef3";
+    hydenix-nixpkgs.url = "github:nixos/nixpkgs/b3da656039dc7a6240f27b2ef8cc6a3ef3bccae7";
 
     home-manager = {
       url = "github:nix-community/home-manager";
